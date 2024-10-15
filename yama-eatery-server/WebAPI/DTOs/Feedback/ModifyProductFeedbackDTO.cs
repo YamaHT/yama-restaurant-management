2b3d961b1da75049b73712526b02931709d3e59a
sinl <sinlthai570@gmail.com>
2024-10-16 04:58:03 +0700

    Fix wrong merge

---
+++ yama-eatery-server/WebAPI/DTOs/Feedback/ModifyProductFeedbackDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.DTOs.Feedback
+{
+    public class ModifyProductFeedbackDTO
+    {
+        public int userId { get; set; }
+        public int productId { get; set; }
+        public string? Message { get; set; }
+        public double Rating { get; set; }
+    }
+}
