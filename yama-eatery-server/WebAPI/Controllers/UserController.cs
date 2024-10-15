b1f8bcfc66f65c2b45b0830c6c7813a72f73caec
khanh <nguyenkhanhvip22@gmail.com>
2024-10-16 00:04:32 +0700

    back-end update

--- yama-eatery-server/WebAPI/Controllers/UserController.cs
+++ yama-eatery-server/WebAPI/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebAPI.DTOs.User;
+using WebAPI.Models.Enums;
 using WebAPI.Utils;
+using WebAPI.Utils.Exceptions;
 
 namespace WebAPI.Controllers
 {
@@ -10,15 +12,51 @@ namespace WebAPI.Controllers
         public async Task<IActionResult> ChangePassword([FromBody] UserChangePasswordDTO userChangePasswordDTO)
         {
             var user = await _unitOfWork.GetUserFromHttpContextAsync(HttpContext);
-
             if (!CryptoUtils.IsPasswordCorrect(userChangePasswordDTO.Password, user.Password))
             {
                 throw new InvalidDataException("Oldpassword is not corrected");
             }
             user.Password = CryptoUtils.EncryptPassword(userChangePasswordDTO.NewPassword);
             _unitOfWork.UserRepository.Update(user);
-            await _unitOfWork.SaveChangeAsync(); ;
+            await _unitOfWork.SaveChangeAsync();
             return Ok(new { success = "Change Password successfully" });
+        }
+        [HttpGet]
+        public async Task<IActionResult> CancelBooking(int bookingID)
+        {
+            var booking = await _unitOfWork.BookingRepository.GetByIdAsync(bookingID);
+            if (booking == null)
+            {
+                throw new DataNotFoundException($"Booking with ID {bookingID} not found.");
+            }
+            if (Enum.TryParse(booking.BookingStatus.ToString(), out BookingStatusEnum status) && (status == BookingStatusEnum.Undeposited || status == BookingStatusEnum.Booking))
+            {
+                _unitOfWork.BookingRepository.Remove(booking);
+                await _unitOfWork.SaveChangeAsync();
+                return Ok($"Booking with ID {bookingID} has been canceled successfully.");
+            }
+            else
+            {
+                throw new DataNotFoundException($"Cannot cancel booking. Current status is: {booking.BookingStatus}.");
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllFeedbacks(int? productId = null, int? rating = null)
+        {
+            var feedbacks = await _unitOfWork.FeedbackProductRepository.GetAllAsync();
+            return Ok(feedbacks);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetHistoryBooking(int userId)
+        {
+            var user = await _unitOfWork.UserRepository.GetByIdAsync(userId, ["Bookings"]);
+            return Ok(user);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMyVouchers(int userIds)
+        {
+            var user = await _unitOfWork.UserRepository.GetByIdAsync(userIds, ["UserVouchers"]);
+            return Ok(user);
         }
     }
 }
--- yama-eatery-server/WebAPI/Controllers/VoucherController.cs
+++ yama-eatery-server/WebAPI/Controllers/VoucherController.cs
@@ -6,5 +6,39 @@ namespace WebAPI.Controllers
 {
     public class VoucherController(IUnitOfWork _unitOfWork) : ApiController
     {
+        [HttpGet]
+        public async Task<IActionResult> GetAllVouchers(int? voucherId = null, bool? IsDeleted = null, string name = null, DateOnly? expiredDate = null, int? reducedPercent = null)
+        {
+            var vouchers = await _unitOfWork.VoucherRepository.GetAllAsync();
+            var validVouchers = vouchers
+                .Where(v => v.Quantity > 0 && v.ExpiredDate > DateOnly.FromDateTime(DateTime.Now))
+                .AsQueryable();
+            if (voucherId.HasValue)
+            {
+                validVouchers = validVouchers.Where(v => v.Id == voucherId.Value);
+            }
+            if (IsDeleted.HasValue)
+            {
+                validVouchers = validVouchers.Where(v => v.IsDeleted == IsDeleted.Value);
+            }
+            if (!string.IsNullOrEmpty(name))
+            {
+                validVouchers = validVouchers.Where(v => v.Name.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0);
+            }
+            if (expiredDate.HasValue)
+            {
+                validVouchers = validVouchers.Where(v => v.ExpiredDate <= expiredDate.Value);
+            }
+            if (reducedPercent.HasValue)
+            {
+                validVouchers = validVouchers.Where(v => v.ReducedPercent >= reducedPercent.Value);
+            }
+            var resultVouchers = validVouchers.ToList();
+            if (!resultVouchers.Any())
+            {
+                return NotFound("No valid vouchers found.");
+            }
+            return Ok(resultVouchers);
+        }
     }
 }
