04ed30dc79993b96e9d3a7696a51a64f46733626
QuachKhang <phuckhang1088@gmail.com>
2024-10-22 23:33:06 +0700

    fix add && update staff

---
+++ yama-eatery-server/WebAPI/DTOs/Staff/AddStaffDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace WebAPI.DTOs.Staff
+{
+    public class AddStaffDTO
+    {
+        public List<IFormFile>? ImageFiles { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Phone { get; set; }
+        public string? Gender { get; set; }
+        public int PositionId { get; set; }
+
+
+    }
+}
